5893bef256c29c62b017bfd1b1ac698433c227d8
Reza akbari <r.akbari.dev@gmail.com>
2025-01-22 12:19:00 -0500

    task one

---
+++ IOProject/Form1.Designer.cs
@@ -0,0 +1,145 @@
+﻿namespace IOProject
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.rtbText = new System.Windows.Forms.RichTextBox();
+            this.txtPath = new System.Windows.Forms.TextBox();
+            this.btnRead = new System.Windows.Forms.Button();
+            this.btnCount = new System.Windows.Forms.Button();
+            this.btnWrite = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.lblCount = new System.Windows.Forms.Label();
+            this.txtWordToCount = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // rtbText
+            // 
+            this.rtbText.Location = new System.Drawing.Point(12, 12);
+            this.rtbText.Name = "rtbText";
+            this.rtbText.Size = new System.Drawing.Size(448, 235);
+            this.rtbText.TabIndex = 0;
+            this.rtbText.Text = "";
+            // 
+            // txtPath
+            // 
+            this.txtPath.Location = new System.Drawing.Point(12, 254);
+            this.txtPath.Name = "txtPath";
+            this.txtPath.Size = new System.Drawing.Size(448, 20);
+            this.txtPath.TabIndex = 1;
+            // 
+            // btnRead
+            // 
+            this.btnRead.Location = new System.Drawing.Point(83, 351);
+            this.btnRead.Name = "btnRead";
+            this.btnRead.Size = new System.Drawing.Size(75, 23);
+            this.btnRead.TabIndex = 2;
+            this.btnRead.Text = "Read";
+            this.btnRead.UseVisualStyleBackColor = true;
+            this.btnRead.Click += new System.EventHandler(this.btnRead_Click);
+            // 
+            // btnCount
+            // 
+            this.btnCount.Location = new System.Drawing.Point(164, 351);
+            this.btnCount.Name = "btnCount";
+            this.btnCount.Size = new System.Drawing.Size(75, 23);
+            this.btnCount.TabIndex = 3;
+            this.btnCount.Text = "Count";
+            this.btnCount.UseVisualStyleBackColor = true;
+            this.btnCount.Click += new System.EventHandler(this.btnCount_Click);
+            // 
+            // btnWrite
+            // 
+            this.btnWrite.Location = new System.Drawing.Point(245, 351);
+            this.btnWrite.Name = "btnWrite";
+            this.btnWrite.Size = new System.Drawing.Size(75, 23);
+            this.btnWrite.TabIndex = 4;
+            this.btnWrite.Text = "Write";
+            this.btnWrite.UseVisualStyleBackColor = true;
+            this.btnWrite.Click += new System.EventHandler(this.btnWrite_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(327, 351);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(75, 23);
+            this.btnExit.TabIndex = 5;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // lblCount
+            // 
+            this.lblCount.AutoSize = true;
+            this.lblCount.Location = new System.Drawing.Point(18, 329);
+            this.lblCount.Name = "lblCount";
+            this.lblCount.Size = new System.Drawing.Size(35, 13);
+            this.lblCount.TabIndex = 6;
+            this.lblCount.Text = "label1";
+            // 
+            // txtWordToCount
+            // 
+            this.txtWordToCount.Location = new System.Drawing.Point(13, 281);
+            this.txtWordToCount.Name = "txtWordToCount";
+            this.txtWordToCount.Size = new System.Drawing.Size(447, 20);
+            this.txtWordToCount.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(472, 386);
+            this.Controls.Add(this.txtWordToCount);
+            this.Controls.Add(this.lblCount);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnWrite);
+            this.Controls.Add(this.btnCount);
+            this.Controls.Add(this.btnRead);
+            this.Controls.Add(this.txtPath);
+            this.Controls.Add(this.rtbText);
+            this.Name = "Form1";
+            this.Text = "IO";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox rtbText;
+        private System.Windows.Forms.TextBox txtPath;
+        private System.Windows.Forms.Button btnRead;
+        private System.Windows.Forms.Button btnCount;
+        private System.Windows.Forms.Button btnWrite;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Label lblCount;
+        private System.Windows.Forms.TextBox txtWordToCount;
+    }
+}
+
---
+++ IOProject/Form1.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Drawing;
+using System.IO;
+using System.Windows.Forms;
+
+namespace IOProject
+{
+    public partial class Form1 : Form
+    {
+        private static readonly log4net.ILog log = log4net.LogManager.GetLogger(typeof(Form1));
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // Placeholders
+            txtPath.Text = "Enter Path";
+            txtPath.ForeColor = Color.Gray;
+
+            txtWordToCount.Text = "Enter Word";
+            txtWordToCount.ForeColor = Color.Gray;
+        }
+
+        private void btnRead_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                log.Info("Reading file...");
+                if (txtPath.Text.StartsWith("Enter"))
+                    txtPath.Text = "";
+                if (txtWordToCount.Text.StartsWith("Enter"))
+                    txtWordToCount.Text = "";
+
+                string aPath = txtPath.Text.Trim();
+                string content = File.ReadAllText(aPath);
+                rtbText.Text = content;
+            }
+            catch (Exception ex)
+            {
+                log.Error("Error while reading file: " + ex.Message);
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        // Method to count a given word
+        private int CountWord(string text, string word)
+        {
+            int count = 0;
+            int i = text.IndexOf(word, StringComparison.OrdinalIgnoreCase); // Case-insensitive search
+            while (i != -1)
+            {
+                count++;
+                i = text.IndexOf(word, i + word.Length, StringComparison.OrdinalIgnoreCase);
+            }
+            return count;
+        }
+
+        private void btnCount_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string word = txtWordToCount.Text.Trim();
+                string text = rtbText.Text;
+
+                int countWord = CountWord(text, word);
+                lblCount.Text = $"The word appeared: {countWord} times";
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void btnWrite_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string aPath = txtPath.Text.Trim();
+                using (TextWriter txt = new StreamWriter(aPath))
+                {
+                    txt.Write(rtbText.Text);
+                }
+                lblCount.Text = "File was Updated";
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
